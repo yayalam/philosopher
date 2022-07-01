85a9247e22b2e30539eca24fae8745e23cb7bdaa
yahya_lambarki <86856588+yayalam@users.noreply.github.com>
2022-07-01 21:55:28 +0100

    Add files via upload

---
+++ philo/philo_bonus/life.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   life.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ylambark <ylambark@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/01 06:17:13 by ylambark          #+#    #+#             */
+/*   Updated: 2022/07/01 06:17:14 by ylambark         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosopher_bonus.h"
+
+void	philo_eat(t_const_philo *philo)
+{
+	sem_wait(philo->var->fork);
+	philo_do(philo, philo->id, "has taken a fork");
+	sem_wait(philo->var->fork);
+	philo_do(philo, philo->id, "has taken a fork");
+	philo_do(philo, philo->id, "is eating");
+	my_sleep(philo->var->time_to_eat, philo->var);
+	sem_post(philo->var->fork);
+	sem_post(philo->var->fork);
+	philo->time_last_eat = timestamp();
+	philo->ate++;
+}
+
+void	life(t_const_philo *philo)
+{
+	while (!philo->dead_philo && philo->ate != philo->var->nb_must_eat)
+	{
+		philo_eat(philo);
+		philo_do(philo, philo->id, "is sleeping");
+		my_sleep(philo->var->time_to_sleep, philo->var);
+		philo_do(philo, philo->id, "is thinking");
+	}
+}
+
+void	*philos_process(t_philo *philos, int i)
+{
+	t_const_philo	philo;
+
+	philo.var = philos;
+	philo.id = i + 1;
+	philo.dead_philo = 0;
+	philo.ate = 0;
+	philo.time_last_eat = timestamp();
+	pthread_create(philo.philo_thread, NULL, (void *)check_death, &philo);
+	life(&philo);
+	pthread_detach(*philo.philo_thread);
+	return (NULL);
+}
+
+int	init_sema(t_philo *philo)
+{
+	sem_unlink("philo_forks");
+	sem_unlink("philo_action");
+	philo->fork = sem_open("philo_forks", O_CREAT, 0700, philo->nb_philos);
+	philo->act = sem_open("philo_action", O_CREAT, 0700, 1);
+	if (philo->fork == SEM_FAILED
+		|| philo->act == SEM_FAILED)
+	{
+		destroy_sem(philo);
+		return (0);
+	}
+	return (1);
+}
---
+++ philo/philo_bonus/philo_bonus.c
@@ -0,0 +1,88 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo_bonus.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ylambark <ylambark@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/01 06:16:49 by ylambark          #+#    #+#             */
+/*   Updated: 2022/07/01 06:18:54 by ylambark         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosopher_bonus.h"
+
+void	print_error(int err)
+{
+	if (err == 1)
+		printf("Wrong args.\n");
+	else if (err == 2)
+		printf("Error parsing.\n");
+	else if (err == 3)
+		printf("Error creating threads.\n");
+	return ;
+}
+
+void	destroy_sem(t_philo *philo)
+{
+	sem_close(philo->fork);
+	sem_close(philo->act);
+	sem_unlink("philo_forks");
+	sem_unlink("philo_action");
+}
+
+int	doo(t_philo *philo)
+{
+	int		i;
+
+	i = 0;
+	philo->pid = malloc(sizeof(int) * philo->nb_philos);
+	if (!philo->pid)
+		return (0);
+	create_proc(philo, philo->pid);
+	while (i < philo->nb_philos)
+	{
+		waitpid(philo->pid[i++], 0, 0);
+	}
+	process_kill(philo);
+	destroy_sem(philo);
+	return (1);
+}
+
+int	init_var(t_philo *philo, char **av)
+{
+	philo->nb_philos = ft_atoi(av[1]);
+	philo->time_to_die = ft_atoi(av[2]);
+	philo->time_to_eat = ft_atoi(av[3]);
+	philo->time_to_sleep = ft_atoi(av[4]);
+	philo->dead = 0;
+	philo->time_birth = timestamp();
+	if (philo->nb_philos < 1 || philo->time_to_die < 1
+		|| philo->time_to_eat < 0
+		|| philo->time_to_sleep < 0)
+		return (0);
+	if (av[5])
+	{
+		philo->nb_must_eat = ft_atoi(av[5]);
+		if (philo->nb_must_eat <= 0)
+			return (0);
+	}
+	else
+		philo->nb_must_eat = -1;
+	if (!init_sema(philo))
+		return (0);
+	return (1);
+}
+
+int	main(int ac, char **av)
+{
+	t_philo	philo;
+
+	if (ac != 5 && ac != 6)
+		return (print_error(1), 0);
+	if (!(init_var(&philo, av)))
+		return (print_error(2), 0);
+	if (!(doo(&philo)))
+		return (print_error(3), 0);
+	return (1);
+}
